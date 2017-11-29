a3e8be6885683641a0f1aafcb877edcc40b25a3e
Camden Ko <camdenko@gmail.com>
2017-11-29 13:58:22 -0600

    images are now prepended with img if they start with _

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
 var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
+const rename = require('gulp-rename')
 var del = require('del');
 
-gulp.task('resize', function () {
-    return gulp.src('images/*.*')
+gulp.task('resize', () =>
+    gulp.src('images/*.*')
         .pipe(imageResize({
             width: 1024,
             imageMagick: true
+        }))
+        .pipe(rename((path) => {
+            if (path.basename[0] === '_') {
+                path.basename = `img${path.basename}`
+            }
         }))
         .pipe(gulp.dest('images/fulls'))
         .pipe(imageResize({
             width: 512,
             imageMagick: true
         }))
-        .pipe(gulp.dest('images/thumbs'));
-});
+        .pipe(gulp.dest('images/thumbs')),
+);
 
 gulp.task('del', ['resize'], function () {
     return del(['images/*.*']);
